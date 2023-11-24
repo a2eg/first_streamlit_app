bacd98a9b2c07d34d84d7521961b92b479e0afad
a2eg <a2eg@users.noreply.github.com>
2023-11-24 12:38:02 -0700

    Update streamlit_app.py
    Fixing '  issue with \'

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text('🥗 Kale, Spinash & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinash & Rocket Smoothie')
