0954508c649591f9a3b3b88a79c27d4717cd738f
a2eg <a2eg@users.noreply.github.com>
2023-11-24 12:09:37 -0700

    Update streamlit_app.py
    Adding Menu 3rd item

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 $ Blueberry Oatmeal')
 
 streamlit.text('Kale, Spinash & Rocket Smoothie')
+
+streamlit.text('Kale, Spinash & Rocket Smoothie')
+
+streamlit.text('Hard-Boiled Free-Range Egg')
