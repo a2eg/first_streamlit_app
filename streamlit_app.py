d5cc4112b76b8029aeec4af3cae2fcd18a4ec779
a2eg <a2eg@users.noreply.github.com>
2023-12-03 01:24:25 -0700

    Update streamlit_app.py
    8

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,7 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 #my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+#streamlit.text("Hello from Snowflake:")
 streamlit.text("The fruit load list contains:")
 #streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
