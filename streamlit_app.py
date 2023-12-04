1f38c6c21da2e75e1889612f804e22ab21390d32
a2eg <a2eg@users.noreply.github.com>
2023-12-03 22:39:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,12 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinash & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+# streamlit.title('My Parents New Healthy Diner')
+# streamlit.header('Breakfast Menu')
+# streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+# streamlit.text('🥗 Kale, Spinash & Rocket Smoothie')
+# streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+# streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
