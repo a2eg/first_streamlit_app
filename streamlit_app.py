f75b5595535f45eb17b81b28edb85badebda94c0
a2eg <a2eg@users.noreply.github.com>
2023-12-03 20:29:20 -0700

    Update streamlit_app.py
    Let's Get All the Rows, Not Just One

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,12 +52,13 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
       # my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-
-my_data_row = my_cur.fetchone()
+      #my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
       #streamlit.text("Hello from Snowflake:")
 streamlit.text("The fruit load list contains:")
       #streamlit.text(my_data_row)
-streamlit.dataframe(my_data_row)
+      #streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 
 
